485d4b6b70905cf6260631cdbbc6c5dfbb043c36
Ian <47256454+IanRFerguson@users.noreply.github.com>
2019-06-06 20:39:57 -0400

    Create summaryStats.R

---
+++ summaryStats.R
@@ -0,0 +1,24 @@
+library(readxl)
+
+# The spreadsheet below is generated using 2018_NBA.py script
+basketballData <- read_excel('~/NBA_Stats.xlsx')
+layup <- basketballData[5:27]
+skyhook <- colnames(layup)
+buildFrame <- data.frame(Stat <- NULL,
+                         Min <- numeric(),
+                         Mean <- numeric(),
+                         Max <- numeric())
+
+k <- 1
+j <- 1
+for (m in layup) {
+        buildFrame[k,1] <- skyhook[k]
+        buildFrame[k,2] <- round((min(m)), 2)
+        buildFrame[k,3] <- round((mean(m)), 2)
+        buildFrame[k,4] <- round((max(m)), 2)
+        k <- k + 1
+}
+
+
+write.table(buildFrame, file = "Summary Stats.xlsx", quote = FALSE,
+            row.names = FALSE, col.names = FALSE, sep = "\t")
