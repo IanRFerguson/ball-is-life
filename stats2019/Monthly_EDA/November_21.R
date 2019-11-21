a20bc9fba73def52c263dba391c4ce516ea2d915
Ian <47256454+IanRFerguson@users.noreply.github.com>
2019-11-21 15:46:41 -0500

    Create November_21.R

---
+++ stats2019/Monthly_EDA/November_21.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(readxl)
+library(MASS)
+
+setwd("~/DATA/SPORTS SCIENCE/NBA/2019/statCompiler/")
+
+data <- read_excel("NBA_Stats_2019.xlsx")
+attach(data)
+
+data %>% 
+        group_by(conference) %>% 
+        summarise(teams = n(),
+                  mean.points = mean(points),
+                  mean.win = mean(winpct_tot))
+
+# R^2 = 0.918 ... Adjusted R^2 = 0.704
+win.model <- lm(winpct_tot ~ . -team -conference, data = data)
+
+# Backwards Selection
+# Updated R^2 = 0.911 ... Adjusted R^2 = 0.828
+win.step <- stepAIC(win.model)
+
+grand.points <- mean(points)
+
+data <- data %>% 
+        mutate(pointDiff = points - grand.points)
+
+data.plot <- ggplot(data, aes(x = reorder(team, +points), y = pointDiff, fill = conference)) +
+                geom_col() +
+                coord_flip() +
+                labs(x = "Team", y = "Distance From Avg. Points",
+                     title = "Average Points: 110.347") +
+                scale_fill_brewer(palette = "Set1")
+
+ggsave("1121_PPG.png", plot = data.plot)
+
+data %>% 
+        group_by(conference) %>% 
+        summarise(teams = n(),
+                  above.avg = sum(pointDiff > 0),
+                  below.avg = sum(pointDiff < 0),
+                  def.eff = mean(def_eff))
+
+data.plot2 <- ggplot(data, aes(x = conference, y = off_eff, fill = conference)) +
+                geom_boxplot() +
+                scale_fill_brewer(palette = "Set1") +
+                labs(x = "Conference", y = "Offensive Efficiency")
+
+ggsave("1121_OffEff.png", plot = data.plot2)
+
+data %>% 
+        filter(conference == "West") %>%
+        group_by(team) %>% 
+        summarise(off.eff = off_eff) %>% 
+        arrange(off.eff)
+
+data %>% 
+        filter(conference == "East") %>%
+        group_by(team) %>% 
+        summarise(off.eff = off_eff) %>% 
+        arrange(off.eff)
+
+data.plot3 <- ggplot(data, aes(x = reorder(team, +off_eff), y = off_eff, fill = conference)) +
+                geom_col() +
+                scale_fill_brewer(palette = "Set1") +
+                theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+                labs(x = "Teams", y = "Offensive Efficiency")
+
+ggsave("1121_OffEff_byTeam.png", plot = data.plot3)
